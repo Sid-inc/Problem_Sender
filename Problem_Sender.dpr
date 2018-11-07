1050a9d131666a38307c0b06764a0f1c1e065a4d
Sid-inc <sid-sked@ya.ru>
2018-11-07 20:19:28 +0300

    Начало

---
+++ Problem_Sender.dpr
@@ -0,0 +1,14 @@
+program Problem_Sender;
+
+uses
+  Vcl.Forms,
+  main in 'main.pas' {MainForm};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.MainFormOnTaskbar := True;
+  Application.CreateForm(TMainForm, MainForm);
+  Application.Run;
+end.
